8787932f94d397acb3586a4f64867e2a08ab0c79
springframeworkguru <john@springframework.guru>
2018-07-16 20:20:07 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo Project for Circle CI
+This repository holds code example for Log4J 2 configuration in a Spring Boot application.
+
---
+++ src/main/test/com/stackroute/restaurantspringboot/controller/RestaurantControllerTest.java
@@ -0,0 +1,33 @@
+package com.stackroute.restaurantspringboot.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantController.class)
+public class RestaurantControllerTest {
+    @Autowired
+    private MockMvc movieMockMvc;
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void displayRestaurant() throws Exception{
+        movieMockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello"));
+    }
+}
