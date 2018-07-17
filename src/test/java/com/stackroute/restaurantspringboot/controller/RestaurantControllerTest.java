7eb6a7c2762a1a037ac95f059bfee65b6b9cdd2d
Ximanta <ximanta.sarma@gmail.com>
2018-07-17 08:37:55 +0530

    Corrected folder structure

--- src/test/java/com/stackroute/restaurantspringboot/controller/RestaurantControllerTest.java
+++ src/test/java/com/stackroute/restaurantspringboot/controller/RestaurantControllerTest.java
@@ -1,18 +1,17 @@
 package com.stackroute.restaurantspringboot.controller;
-
+import com.stackroute.restaurantspringboot.controller.RestaurantController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringRunner.class)
